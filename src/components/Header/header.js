f783adbfe8565d95e7523a73f588fb187c404f41
prabhashk123 <sunriseprabhash.bh@gmail.com>
2024-01-20 00:44:33 +0530

    fixed header top navbar

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -37,8 +37,8 @@ function Header(props) {
 
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div className="container-fluid overflow-none" >
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
